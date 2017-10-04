4456a543b6470da9faf6ed4ba308537f268ebd69
zawzaww <mczawzaw12@outlook.com>
2017-10-04 12:29:34 +0630

    TWRP flags: add twrp crypto flags
     Changes to be committed:
    	modified:   BoardConfig.mk
    
    Signed-off-by: ZawZaw <mczawzaw12@outlook.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -50,5 +50,14 @@ TW_EXTERNAL_STORAGE_PATH := "/external_sd"
 TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
 TARGET_NO_SEPARATE_RECOVERY := true
 
+# TWRP Crypto
+TW_INCLUDE_CRYPTO := true
+TW_CRYPTO_FS_TYPE := "ext4"
+TW_CRYPTO_MNT_POINT := "/data"
+TW_CRYPTO_REAL_BLKDEV := "/dev/block/bootdevice/by-name/userdata"
+TW_CRYPTO_FS_OPTIONS := "nosuid,nodev,barrier=1,noauto_da_alloc,discard"
+TW_CRYPTO_FS_FLAGS := "0x00000406"
+TW_CRYPTO_KEY_LOC := "footer"
+
 BOARD_KERNEL_CMDLINE += androidboot.hardware=togari
 BOARD_KERNEL_CMDLINE += mem=1759M
